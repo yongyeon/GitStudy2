432942acf06e8ddb2f4ba74aa5e031b5f86caf67
yongyeon <yykim@andwise.com>
2013-06-12 18:06:13 +0900

    Commit No1 master

---
+++ src/Test_130612/Main.java
@@ -0,0 +1,21 @@
+package Test_130612;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Git Study Start.");
+		System.out.println("Project by yykim.");
+		
+		System.out.println("Test Study");
+		
+		testClass();
+		
+		System.out.println("2013-06-12 Study Start");
+	}
+	
+	public static void testClass() {
+		System.out.println("This Class Name is testClass !!");
+	}
+
+}
